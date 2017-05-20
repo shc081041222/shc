57fb77abedfe62d913917e4f2dc2a75b5527d69a
sunhuachao <274049828@qq.com>
2017-05-20 11:15:22 +0800

    commit answer

--- main/main.js
+++ main/main.js
@@ -1,36 +1,53 @@
-module.exports = function main() {
-    let sequence = new Sequence([6, 9, 15, -2, 92, 11]);
-    console.log(`o) 最小值 = ${sequence.minimum()}
-o) 最大值 = ${sequence.maxmum()}
-o) 元素数量 = ${sequence.num()}
-o) 平均值 = ${sequence.ave()}`);
+const loadAllItems = require('./loadAllItems.js');
+
+module.exports = function main(inputs) {
+	
+	var itembarcode;  
+	var item_list=[];      
+	var list='***<没钱赚商店>购物清单***\n';  
+	var sum=0;
+    let item_Table = loadAllItems();	
+	for(var i=0;i<inputs.length;i++){  
+		itembarcode=inputs[i];  
+		if(item_list.length==0){ 
+			for(var k = 0; k < item_Table.length; k++){
+				if(itembarcode == item_Table[k].barcode){
+					item_Table[k].count = 1;
+					item_list.push(item_Table[k]);
+					
+				}
+			}          
+		}  
+		else{
+			var temp = 0;
+			for(var j=0;j<item_list.length;j++){  
+				if(itembarcode == item_list[j].barcode){  
+					item_list[j].count++; 
+					temp++;
+				}  
+			}
+			if(temp == 0){
+				for(var k = 0; k < item_Table.length; k++){
+					if(itembarcode == item_Table[k].barcode){
+						item_Table[k].count = 1;
+						item_list.push(item_Table[k]);
+					
+					}
+				}  				
+			}
+		}  
+    } 
+	
+	for(var j = 0; j < item_list.length; j++){
+		sum += item_list[j].count*item_list[j].price;
+		
+		list = list  + '名称：'+item_list[j].name+'，'+'数量：'+item_list[j].count+item_list[j].unit+'，'+'单价：'+item_list[j].price.toFixed(2)+'(元)' +'，'+'小计：'+(item_list[j].count*item_list[j].price).toFixed(2)+'(元)'+'\n';  
+	}
+	
+	list = list + '----------------------\n' + '总计：' + sum.toFixed(2) +'(元)' + '\n' + '**********************';
+	
+	
+	
+    console.log("Debug Info");
+    return list;
 };
-
-
-class Sequence {
-  constructor(input) {
-    this.input=input;
-  }
-
-  minimum() {
-    return Math.min.apply( Math, this.input );
-    return min;
-  }
-
-  maxmum() {
-    return Math.max.apply( Math, this.input );
-  }
-  
-  num() {
-    return this.input.length;
-  }
-  
-  ave() {
-    var sum=0;
-    for(var i=0;i<this.input.length;i++){
-        sum+=this.input[i];
-    }
-    var ave=sum/this.input.length;
-    return ave.toFixed(2);
-  }
-}
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,12 +1,35 @@
-const main = require('../main/main');
+const printInventory = require('../main/main');
 
-describe('main()', () => {
-    it('should print stats', () => {
-        spyOn(console, 'log');
-        main();
-        expect(console.log).toHaveBeenCalledWith(`o) 最小值 = -2
-o) 最大值 = 92
-o) 元素数量 = 6
-o) 平均值 = 21.83`);
+describe('pos', () => {
+    var inputs;
+
+    beforeEach(() => {
+        inputs = [
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000000',
+            'ITEM000001',
+            'ITEM000001',
+            'ITEM000004'
+        ];
+    });
+
+    it('should print correct text', () => {
+
+
+        let actualText = printInventory(inputs);
+
+        var expectText =
+            '***<没钱赚商店>购物清单***\n' +
+            '名称：可口可乐，数量：5瓶，单价：3.00(元)，小计：15.00(元)\n' +
+            '名称：雪碧，数量：2瓶，单价：3.00(元)，小计：6.00(元)\n' +
+            '名称：电池，数量：1个，单价：2.00(元)，小计：2.00(元)\n' +
+            '----------------------\n' +
+            '总计：23.00(元)\n' +
+            '**********************';
+
+        expect(actualText).toBe(expectText);
     });
 });
